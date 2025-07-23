be9358fc6dcb04da98f21e6bf5ccd0e081e8b06b
Chae-eun Lee <cheun9325@naver.com>
2025-07-23 23:40:06 +0900

    [Silver III] Title: 소수 구하기, Time: 432 ms, Memory: 18736 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int M = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
