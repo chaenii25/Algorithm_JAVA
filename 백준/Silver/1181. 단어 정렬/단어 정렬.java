0185d7ca182b7f310c76db507196ccc5aad5ead1
Chae-eun Lee <cheun9325@naver.com>
2025-09-03 22:24:32 +0900

    [Silver V] Title: 단어 정렬, Time: 440 ms, Memory: 22164 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bf.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < num; i++) {
+            set.add(bf.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        Collections.sort(list, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            } else {
+                return a.compareTo(b);
+            }
+        });
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
