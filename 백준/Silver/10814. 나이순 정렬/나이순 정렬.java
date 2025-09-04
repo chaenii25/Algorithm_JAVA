10f20965de8cc14d8d3a1822430d2b20a5d6150b
Chae-eun Lee <cheun9325@naver.com>
2025-09-04 23:00:25 +0900

    [Silver V] Title: 나이순 정렬, Time: 1260 ms, Memory: 64144 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+            String[] input = br.readLine().split(" ");
+            int age = Integer.parseInt(input[0]);
+            String name = input[1];
+            people.add(new Person(age, name));
+        }
+
+        Collections.sort(people, (a, b) -> {
+         return Integer.compare(a.age, b.age);
+        });
+
+        for (Person person : people) {
+            System.out.println(person.age + " " + person.name);
+        }
+    }
+}
+
+class Person {
+    int age;
+    String name;
+
+    Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
