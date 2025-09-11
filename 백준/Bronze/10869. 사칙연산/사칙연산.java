8bab863ca94727b5fde501d38eac088e7a97b98d
Chae-eun Lee <cheun9325@naver.com>
2025-09-11 20:10:07 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] str = br.readLine().split(" ");
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[1]);
+        
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}
