b0a85f479e81cfd1223341d8bd46fa88134f9589
Chae-eun Lee <cheun9325@naver.com>
2025-09-19 22:28:54 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < num; i++) {
+            String title = br.readLine();
+
+            if(map.containsKey(title)){
+                map.put(title,map.get(title)+1);
+            } else {
+                map.put(title,1);
+            }
+        }
+
+        String best = "";
+        int max = 0;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String title = entry.getKey();
+            int count = entry.getValue();
+
+            if (count > max) {
+                max = count;
+                best = title;
+            } else if (count == max) {
+                if (title.compareTo(best) < 0) {
+                    best = title;
+                }
+            }
+        }
+        
+        System.out.println(best);
+    }
+}
